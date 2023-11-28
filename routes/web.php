6aa9a7354682712b6a44d73e89dbe2031d796435
HossainAbedy <abedy.ewu@gmail.com>
2023-11-28 13:19:58 +0600

    Login/Registe with Passport

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/dashboard/clients', function (Request $request) {
+    return view('clients', [
+        'clients' => $request->user()->clients
+    ]);
+})->middleware(['auth'])->name('dashboard.clients');
+
+
+// ->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
